0496e7ea6950fbc8630cccc762694400748cbaae
kiran sekhar panda <kiransekharp229@gmail.com>
2026-08-17 10:17:26 +0530

    added basic file structure and create controller

---
+++ src/main/java/com/sjma/SmartJobManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sjma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartJobManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartJobManagementApplication.class, args);
+	}
+
+}
